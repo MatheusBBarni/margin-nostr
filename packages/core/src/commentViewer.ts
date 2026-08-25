15ee6499d7a46f8a3e8397c60e83c8b4819d2519
MatheusBBarni <brehm.matheus@hotmail.com>
2026-08-25 20:40:23 -0300

    refactor: encode njump nevent with KIND_COMMENT

--- packages/core/src/commentViewer.ts
+++ packages/core/src/commentViewer.ts
@@ -1,6 +1,6 @@
 import { neventEncode } from "nostr-tools/nip19"
-import type { VerifiedComment } from "./events"
+import { KIND_COMMENT, type VerifiedComment } from "./events"
 
 export function commentViewerUrl(comment: Pick<VerifiedComment, "id" | "pubkey">): string {
-  return `https://njump.me/${neventEncode({ id: comment.id, author: comment.pubkey, kind: 1111 })}`
+  return `https://njump.me/${neventEncode({ id: comment.id, author: comment.pubkey, kind: KIND_COMMENT })}`
 }
