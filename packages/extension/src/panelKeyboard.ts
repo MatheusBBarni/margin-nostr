7b4b88276e0c24b3c6d75014d8fab2418bb75a4f
MatheusBBarni <brehm.matheus@hotmail.com>
2026-08-25 14:14:14 -0300

    feat: focus compose when signed in on a room

--- packages/extension/src/panelKeyboard.ts
+++ packages/extension/src/panelKeyboard.ts
@@ -1,5 +1,7 @@
 export type PanelCommand = "open" | "close" | "focus"
 
+export type FocusTarget = "wait" | "compose" | "auth" | "none"
+
 export function decidePanelCommand(input: {
   open: boolean
   canClose: boolean
@@ -11,4 +13,12 @@ export function decidePanelCommand(input: {
     return "close"
   }
   return "focus"
+}
+
+export function decideFocusTarget(_input: {
+  signerReady: boolean
+  hasSigner: boolean
+  roomFocusable: boolean
+}): FocusTarget {
+  return "compose"
 }
